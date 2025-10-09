2f212212f53d271a3d7692d9ee7f6221c91a5a24
Kosk0l <kosk0l@yandex.ru>
2025-10-09 14:59:57 +0300

    sqlite3-migration

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -1,19 +1,27 @@
-package migrator
+package main
 
 import (
 	"errors"
 	"flag"
 	"fmt"
 
+	// Драйвер миграции основной
 	"github.com/golang-migrate/migrate/v4"
+
+	// Драйвер для получения миграции из файла
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	// Драйвер для выполнения миграция sqlite3
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
 )
 
+// go run ./cmd/migrator --storage-path=./storage/sso.db --migrations-path=./migrations
 func main() {
 	// Путь Бд // путь файлов Миграции // Имя таблицы, куда мигратор будет сохранять изменения
 	var storagePath, migrationsPath, migrationsTable string
 
 	flag.StringVar(&storagePath, "storage-path", "", "path to storages")
-	flag.StringVar(&migrationsPath,"migratiions-path", "", "path to migrations")
+	flag.StringVar(&migrationsPath,"migrations-path", "", "path to migrations")
 	flag.StringVar(&migrationsTable,"migrations-table", "migrations", "name of migration")
 	flag.Parse()
 
